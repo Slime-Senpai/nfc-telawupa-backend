6ae2d590e09dcbc94a87ba8ea261f84fadb196b0
Slime-Senpai <mrthetawtute@hotmail.fr>
2022-02-28 15:11:43 +0100

    fix api

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
+const userRouter = require('./routes/users');
+const roomRouter = require('./routes/room');
+const scanRouter = require('./routes/scan');
 
 const app = express();
 
@@ -20,7 +22,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/user', userRouter);
+app.use('/room', roomRouter);
+app.use('/scan', scanRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
