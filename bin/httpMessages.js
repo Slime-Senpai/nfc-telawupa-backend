32588d4c0e50362593efcd8d9918e2fd39969792
Slime-Senpai <mrthetawtute@hotmail.fr>
2022-02-28 15:18:20 +0100

    Fix name message

--- bin/httpMessages.js
+++ bin/httpMessages.js
@@ -1,13 +1,13 @@
 class HTTPMessages {}
 
-HTTPMessages.Created = { code: 20100, val: 'Created!' };
+HTTPMessages.Created = { code: 20100, message: 'Created!' };
 
-HTTPMessages.BadRequest = { code: 40000, val: 'Bad Request!' };
-HTTPMessages.Unauthorized = { code: 40100, val: 'Unauthorized!' };
-HTTPMessages.NotFound = { code: 40400, val: 'Not found!' };
-HTTPMessages.RoomNotFound = { code: 40401, val: 'Room not found!' };
-HTTPMessages.UserNotFound = { code: 40402, val: 'User not found!' };
+HTTPMessages.BadRequest = { code: 40000, message: 'Bad Request!' };
+HTTPMessages.Unauthorized = { code: 40100, message: 'Unauthorized!' };
+HTTPMessages.NotFound = { code: 40400, message: 'Not found!' };
+HTTPMessages.RoomNotFound = { code: 40401, message: 'Room not found!' };
+HTTPMessages.UserNotFound = { code: 40402, message: 'User not found!' };
 
-HTTPMessages.InternalServerError = { code: 50000, val: 'Internal Server Error!' };
+HTTPMessages.InternalServerError = { code: 50000, message: 'Internal Server Error!' };
 
 module.exports = HTTPMessages;
